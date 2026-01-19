c349c800ab4dfcd0d3ae9f8255bce8c792f2ba9c
tvorogo <lesadumsev@gmail.com>
2026-01-19 13:19:03 +0400

    Delete installer/OnlineUpdater.cmd

--- installer/OnlineUpdater.cmd
+++
@@ -1,5 +0,0 @@
-@echo off 
-(NET FILE||(powershell -command Start-Process '%0' -Verb runAs -ArgumentList '%* '&EXIT /B))>NUL 2>&1 
-pushd "%~dp0" && cd %~dp0 
-.\tools\DriverUpdater\%PROCESSOR_ARCHITECTURE%\DriverUpdater.exe -r . -d .\definitions\Desktop\ARM64\Internal\nabu.xml 
-pause 
